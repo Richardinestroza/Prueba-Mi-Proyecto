673e6d25fccc50074c6dbd22f0dba32f951caf14
Alumno <alumno@localhost.localdomain>
2014-01-29 08:28:22 -0600

    Subida proyecto

---
+++ src/Miproyecto11.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class Miproyecto11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Esto es para que muestre la ejecucion del programa
+   System.out.print("Hola mundo");
+   Scanner mi_scanner=new Scanner(System.in);
+   int mi_variable=mi_scanner.nextInt();
+   System.out.println(mi_variable);
+   if(mi_variable==10)
+   {
+	   System.out.println("es un 10");
+   }
+   System.out.println("Fin de programa");
+	}
+
+}
